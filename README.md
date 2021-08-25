86a74cf1f21f3f28c15ed981efc49aa8371cc521
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-08-25 12:22:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<img src="https://i.imgur.com/ggCPH8M.jpg">
+
+***
+
+# After SEIR Daily Code Challenges
+
+***
+## Technologies Used
+* HTML5
+* CSS3
+* Javascript
+* Python
+
+
---
+++ multiply.js
@@ -0,0 +1,13 @@
+/* ----------------------------------------------------------------------
+Challenge: this function is not executing properly, find out why
+
+function multiply(a, b){
+   a * b
+} 
+---------------------------------------------------------------------- */
+
+function multiply(a, b){
+    return a * b
+  }
+console.log(multiply(5, 6));
+
